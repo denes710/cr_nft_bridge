224c65b2e3ffbddd164911ddcad92ba9acf76bb0
Denes Fekete <denes710@gmail.com>
2023-03-12 13:18:42 +0100

    progress: IContractMap interface added

---
+++ contracts/interfaces/IContractMap.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+// FIXME comments
+/**
+ * @notice This interface stores the pairs of the ERC721 smart contracts.
+ */
+interface IContractMap {
+    event PairAdded(address indexed _local, address indexed _remote);
+
+    function addPair(address _local, address _remote) external;
+
+    function getRemote(address _local) external view returns (address);
+
+    function getLocal(address _remote) external view returns (address);
+}
