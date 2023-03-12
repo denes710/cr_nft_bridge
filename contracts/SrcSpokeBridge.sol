c62f9d6f4d770fccf34ef7296303cfc5edb4b789
Denes Fekete <denes710@gmail.com>
2023-03-12 17:28:20 +0100

    progress: functionalities of biding added

--- contracts/SrcSpokeBridge.sol
+++ contracts/SrcSpokeBridge.sol
@@ -1,9 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
+import {IContractMap} from "./interfaces/IContractMap.sol";
 import {ISrcSpokeBridge} from "./interfaces/ISrcSpokeBridge.sol";
 
 import {Ownable} from "openzeppelin-solidity/contracts/access/Ownable.sol";
+import {ERC721} from "openzeppelin-solidity/contracts/token/ERC721/ERC721.sol";
 import {Counters} from "openzeppelin-solidity/contracts/utils/Counters.sol";
 
 // FIXME comments
@@ -78,12 +80,32 @@ contract SrcSpokeBridge is ISrcSpokeBridge, Ownable {
         _;
     }
 
-    function createBid() public override payable {
-        // TODO
+    function createBid(address _receiver, uint256 _tokenId, address _erc721Contract, uint32 _chainId) public override payable {
+        require(msg.value > 0, "SrcSpokenBridge: there is no fee for relayers!");
+
+        ERC721(_erc721Contract).safeTransferFrom(msg.sender, address(this), _tokenId);
+
+        outgoingBids[id.current()] = Bid({
+            id:id.current(),
+            status:BidStatus.Created,
+            fee:uint16(msg.value),
+            maker:_msgSender(),
+            receiver:_receiver,
+            tokenId:_tokenId,
+            erc721Contract:_erc721Contract,
+            chainId:_chainId,
+            timeOfBuying:0,
+            buyer:address(0)
+        });
+
+        id.increment();
     }
 
-    function buyBid(uint256 _bidId) public override {
-        // TODO
+    function buyBid(uint256 _bidId) public override onlyActiveRelayer() {
+        require(outgoingBids[_bidId].status == BidStatus.Created, "SrcSpokeBridge: bid does not have Created state");
+        outgoingBids[_bidId].status = BidStatus.Bought;
+        outgoingBids[_bidId].buyer = _msgSender();
+        outgoingBids[_bidId].timeOfBuying = block.timestamp;
     }
 
     function challengeUnlocking(uint256 _bidId) public override payable {
